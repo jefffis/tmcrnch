b9f938cc694ea9c81911ee2278312cb6dbd6cbb7
Jeff Doan <jeffdoan@Jeffs-MacBook-Pro-2.local>
2013-02-25 16:12:46 -0500

    Update and move to PHP

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Testttt::Application.config.secret_token = '0005b18f19de812c0b36abcb32e21d1cd4c2b66966b8a505d0ba3f03e2c53a2e765b08fdeecc8df6216991a45fa1c3eadd8413262bea61328e9eb5f6e94d0bc1'
