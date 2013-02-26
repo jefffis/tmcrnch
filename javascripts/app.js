a4c5b62287dd05b68ed07a06d7c098b2eb8fa0b1
Jeff Doan <jeffdoan@Jeffs-MacBook-Pro-2.local>
2013-02-26 10:34:36 -0500

    Add in styles

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,5 +1,9 @@
 if (Modernizr.localstorage) {
 	
+	$(function(){
+
+	});
+
 	var form = $('#form');
 
 	var email_1 = $('#email_1');
@@ -18,41 +22,52 @@ if (Modernizr.localstorage) {
 
 	email_1.on('change',function(){
 		localStorage.setItem('email_1',email_1.val());
-		submit.prop('disabled',false).css('opacity',1);
+		submit.prop('disabled',false);
+		if(email_1.val()==''){
+			email_1_clear.css('display','none');
+		}else{
+			email_1_clear.css('display','block');	
+		}
 	});
+
 	email_1_clear.on('click',function(){
 		localStorage.setItem('email_1','');
 		email_1.val('');
+		email_1_clear.css('display','none');
 	});
 	email_1.val(localStorage.getItem('email_1'));
 	
 	email_2.on('change',function(){
 		localStorage.setItem('email_2',email_2.val());
-		submit.prop('disabled',false).css('opacity',1);
+		submit.prop('disabled',false);
+		email_2_clear.css('display','block');
 	});
 	email_2_clear.on('click',function(){
 		localStorage.setItem('email_2','');
 		email_2.val('');
+		email_2_clear.css('display','none');
 	});
 	email_2.val(localStorage.getItem('email_2'));
 
 	email_3.on('change',function(){
 		localStorage.setItem('email_3',email_3.val());
-		submit.prop('disabled',false).css('opacity',1);
+		submit.prop('disabled',false);
+		email_3_clear.css('display','block');
 	});
 	email_3_clear.on('click',function(){
 		localStorage.setItem('email_3','');
 		email_3.val('');
+		email_3_clear.css('display','none');
 	});
 	email_3.val(localStorage.getItem('email_3'));
 
 	if((email_1.val()=='') && (email_2.val()=='') && (email_3.val()=='')){
-		submit.prop('disabled',true).css('opacity',.5);
+		submit.prop('disabled',true);
 	}
 
 	form.on('submit',function(){
 	
-		if((message_type.filter(':checked').length==0)||(message_template.filter(':checked').length==0)){
+		if((message_type.filter(':checked').length==0)){
 			return false;
 		}else{
 			
